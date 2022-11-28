23010fdbad1a5690fa27a61b0b580500dbac02d2
Michael Maurer <91212798+maurermi@users.noreply.github.com>
2022-11-28 01:49:02 -0500

    Update main.cpp

--- CPU_SERIAL_V1/src/main.cpp
+++ CPU_SERIAL_V1/src/main.cpp
@@ -6,17 +6,23 @@
 
 #define DIFFICULTY 0xff000000
 
+// time program
 double CLOCK() {
     struct timespec t;
     clock_gettime(CLOCK_MONOTONIC, &t);
     return (t.tv_sec * 1000)+(t.tv_nsec*1e-6);
 }
 
+// determine if the hash is < difficulty
 bool checkVal(uint8_t* hash) {
 	uint32_t diff = DIFFICULTY;
+	// cast difficulty to be a string of uint8_t's like the SHA256 library does
 	uint8_t * d = static_cast<uint8_t*>(static_cast<void*>(&diff));
 
 	for(int i = 0; i < 8; i++) {
+		// compare each uint8_t within the hash to those in difficulty
+		// if hash is ever smaller, this hash wins
+		// if hash is ever larger, it loses (if they are the same, it loses)
 		if(*(d+i) > *(hash + i)) {
 			return true;
 		}
@@ -38,6 +44,7 @@ int main(int argc, char ** argv) {
   uint32_t result[8];
   *val = 1;
 	start = CLOCK();
+	// continue until hash is found
 	while(!solved) {
 		sha.update(reinterpret_cast<char*>(val));
 		digest = sha.digest();
